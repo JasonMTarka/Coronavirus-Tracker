53650faec8f86b96af17a6e7dc41c55b7e437565
JasonTarka <71971736+JasonMTarka@users.noreply.github.com>
2021-03-24 08:27:16 +0900

    Refactoring
    - Refactored existing functions into objects in order to improve code reusability

--- coronavirus_reader.py
+++ coronavirus_reader.py
@@ -3,80 +3,84 @@ from matplotlib import pyplot as plt
 import numpy as np
 import re
 
-def coronavirus_data_reader():
-	with open("coronavirus_data.csv") as file:
-		csv_reader = reader(file)
-		return list(csv_reader)
+class COVID19_Reader:
+	def __init__(self):
+		self.data = []
 
-def graph(data):
-	plt.style.use("ggplot")
-	plt.xlabel("Dates")
-	plt.ylabel("Number of New Cases")
-	plt.title("Number of Coronavirus Cases in Dayton and Tokyo")
+	def data_reader(self):
+		with open("coronavirus_data.csv") as file:
+			csv_reader = reader(file)
+			self.data = list(csv_reader)
+			return self.data
 	
-	x_dates = []
-	y_cases_dayton = []
-	y_cases_tokyo = []
+	def print_cases(self, data):
+		todays_data = data[len(data)-1] #data[len(data)-2]
+		# The above commented out code allows you to see yesterday's data instead.
 
-	for row in data[1:]:
-		x_dates.append(row[0])
-		if row[1] != "NULL":
-			y_cases_dayton.append(int(row[1]))
-		else:
-			y_cases_dayton.append(0)
-		if row[2] != "NULL":
-			y_cases_tokyo.append(int(row[2]))
-		else:
-			y_cases_tokyo.append(0)	
+		# Below is a regex which will match the day, month, and year that the data was collected.
+		date_analysis = re.compile(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d\d?)")
+		match = date_analysis.search(todays_data[0])
+		MONTHS = {
+			"01" : "January",
+			"02" : "February",
+			"03" : "March",
+			"04" : "April",
+			"05" : "May",
+			"06" : "June",
+			"07" : "July",
+			"08" : "August",
+			"09" : "September",
+			"10" : "October",
+			"11" : "November",
+			"12" : "December"
+		}
 
-	x_indexes = np.arange(len(x_dates)) # Creates a numpy array which contains the indexes of the dates, using the indexes to display the data instead of the data itself
+		# Below converts the regex groups into variables so that they can be included in the final f-string.
+		month = MONTHS[match.group("month")]
+		day = match.group("day")
+		year = match.group("year")
 
-	plt.plot(x_dates, y_cases_dayton, marker="o", label="Dayton") 
-	plt.plot(x_dates, y_cases_tokyo, marker="o", label="Tokyo")
+		tokyo_new_cases = todays_data[2]
+		dayton_new_cases = todays_data[1]
+		print(f"The number of new cases in Tokyo on {month} {day}, {year} was {tokyo_new_cases}.")
+		print(f"The number of new cases in Dayton was {dayton_new_cases}.")
 
-	plt.xticks(ticks=x_indexes, labels=x_dates) # Sets ticks equal to indexes
-	plt.locator_params(axis="x", nbins=5) # Sets the amount of x labels which display
+	def graph(self, data):
+		plt.style.use("ggplot")
+		plt.xlabel("Dates") 
+		plt.ylabel("Number of New Cases") 
+		plt.title("Number of Coronavirus Cases in Dayton and Tokyo")
+		
+		x_dates = []
+		y_cases_dayton = []
+		y_cases_tokyo = []
 
-	plt.legend()
-	plt.tight_layout()
-	plt.show()
+		for row in data[1:]:
+			x_dates.append(row[0])
+			if row[1] != "NULL":
+				y_cases_dayton.append(int(row[1]))
+			else:
+				y_cases_dayton.append(0)
+			if row[2] != "NULL":
+				y_cases_tokyo.append(int(row[2]))
+			else:
+				y_cases_tokyo.append(0)	
 
-def print_todays_cases(data):
-	todays_data = data[len(data)-1] #data[len(data)-2]
-	# The above commented out code allows you to see yesterday's data instead.
+		x_indexes = np.arange(len(x_dates)) # Creates a numpy array which contains the indexes of the dates, using the indexes to display the data instead of the data itself
 
-	# Below is a regex which will match the day, month, and year that the data was collected.
-	date_analysis = re.compile(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d\d?)")
-	match = date_analysis.search(todays_data[0])
-	MONTHS = {
-		"01" : "January",
-		"02" : "February",
-		"03" : "March",
-		"04" : "April",
-		"05" : "May",
-		"06" : "June",
-		"07" : "July",
-		"08" : "August",
-		"09" : "September",
-		"10" : "October",
-		"11" : "November",
-		"12" : "December"
-	}
+		plt.plot(x_dates, y_cases_dayton, marker="o", label="Dayton") 
+		plt.plot(x_dates, y_cases_tokyo, marker="o", label="Tokyo")
 
-	# Below converts the regex groups into variables so that they can be included in the final f-string.
-	month = MONTHS[match.group("month")]
-	day = match.group("day")
-	year = match.group("year")
+		plt.xticks(ticks=x_indexes, labels=x_dates) # Sets ticks equal to indexes
+		plt.locator_params(axis="x", nbins=5) # Sets the amount of x labels which display
 
-	tokyo_new_cases = todays_data[2]
-	dayton_new_cases = todays_data[1]
-	print(f"The number of new cases in Tokyo on {month} {day}, {year} was {tokyo_new_cases}.")
-	print(f"The number of new cases in Dayton was {dayton_new_cases}.")
+		plt.legend()
+		plt.tight_layout()
+		plt.show()
 
-def main():
-	data = coronavirus_data_reader()
-	print_todays_cases(data)
-	graph(data)
+if __name__ == "__main__":	
+	cvd_reader = COVID19_Reader()
+	data = cvd_reader.data_reader()
 
-if __name__ == "__main__":
-	main()
+	cvd_reader.print_cases(data)
+	cvd_reader.graph(data)
--- coronavirus_scraper.py
+++ coronavirus_scraper.py
@@ -12,48 +12,53 @@ from time import sleep
 # For viewing the data, run the separate coronavirus_reader.py script. 
 
 # For my purposes I define Dayton as two counties; Montgomery and Greene.
-def get_dayton_stats():
 
-	# Dayton information is taken from two separate pages on the New York Times site, so this function finds
-	# the case number for each county and strips unnecessary information from each
-	def nytimes_scraper(url):
-		ohio_response = requests.get(url)
-		ohio_soup = BeautifulSoup(ohio_response.text, "html.parser")
-		return int(ohio_soup.find(id="cases").find_all("strong")[1].get_text().replace(" cases per day",""))
+class COVID19_Scraper:
+	def __init__(self):
+		self.date = date.today()
 
-	new_greene_cases = nytimes_scraper("https://www.nytimes.com/interactive/2021/us/greene-ohio-covid-cases.html")
-	sleep(1)
-	new_montgomery_cases = nytimes_scraper("https://www.nytimes.com/interactive/2021/us/montgomery-ohio-covid-cases.html")
+	def get_dayton_stats(self):
+		# Dayton information is taken from two separate pages on the New York Times site, so this function finds
+		# the case number for each county and strips unnecessary information from each
+		def nytimes_scraper(url):
+			ohio_response = requests.get(url)
+			ohio_soup = BeautifulSoup(ohio_response.text, "html.parser")
+			return int(ohio_soup.find(id="cases").find_all("strong")[1].get_text().replace(" cases per day",""))
 
-	dayton_total = new_greene_cases + new_montgomery_cases
+		new_greene_cases = nytimes_scraper("https://www.nytimes.com/interactive/2021/us/greene-ohio-covid-cases.html")
+		sleep(1)
+		new_montgomery_cases = nytimes_scraper("https://www.nytimes.com/interactive/2021/us/montgomery-ohio-covid-cases.html")
 
-	#If the totals have not yet been updated, this will write NULL to the csv field instead
-	if dayton_total == 0:
-		dayton_total = "NULL"
-	return dayton_total
+		dayton_total = new_greene_cases + new_montgomery_cases
 
-def get_tokyo_stats():
-	url = "https://stopcovid19.metro.tokyo.lg.jp/en"
-	tokyo_response = requests.get(url)
-	tokyo_soup = BeautifulSoup(tokyo_response.text, "html.parser")
+		#If the totals have not yet been updated, this will return the string "NULL" 
+		if dayton_total == 0:
+			return "NULL"
+		return dayton_total
 
-	tokyo_total = int(tokyo_soup.find(class_="InfectionMedicalcareprovisionStatus-description").span.get_text().replace("人",""))
+	def get_tokyo_stats(self):
+		url = "https://stopcovid19.metro.tokyo.lg.jp/en"
+		tokyo_response = requests.get(url)
+		tokyo_soup = BeautifulSoup(tokyo_response.text, "html.parser")
 
-	if tokyo_total == 0:
-		tokyo_total = "NULL"
-	return tokyo_total
+		tokyo_total = int(tokyo_soup.find(class_="InfectionMedicalcareprovisionStatus-description").span.get_text().replace("人",""))
 
-# Records the number of new cases in Dayton and Tokyo into a csv file called coronavirus_data.csv
-def write_to_csv(new_dayton_cases, new_tokyo_cases):
-	today = date.today()
-	with open("coronavirus_data.csv","a", newline="") as file:
-		csv_writer = writer(file)
-		csv_writer.writerow([today, new_dayton_cases, new_tokyo_cases])
+		if tokyo_total == 0:
+			return "NULL"
+		return tokyo_total
 
-def main():
-	dayton_total = get_dayton_stats()
-	tokyo_total = get_tokyo_stats()
-	write_to_csv(dayton_total, tokyo_total)
+	def package_data(self, dayton_total, tokyo_total):
+		today = date.today().strftime("%d-%m-%y")
+		return {"Date": today, "Dayton": dayton_total, "Tokyo": tokyo_total}
+
+	# Records the number of new cases in Dayton and Tokyo into a csv file called coronavirus_data.csv
+	def write_to_csv(self,date, new_dayton_cases, new_tokyo_cases):
+		with open("coronavirus_data.csv","a", newline="") as file:
+			csv_writer = writer(file)
+			csv_writer.writerow([date, new_dayton_cases, new_tokyo_cases])
 
 if __name__ == "__main__":
-	main()
+	
+	scraper = COVID19_Scraper()
+	todays_values = scraper.package_data(scraper.get_dayton_stats(), scraper.get_tokyo_stats())
+	scraper.write_to_csv(todays_values["Date"], todays_values["Dayton"], todays_values["Tokyo"])
